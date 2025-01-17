7ba71f04c86fb1c170e2f90421559c77bc3617fe
elguns <elguns@goldenpay.az>
2025-01-17 10:42:59 +0400

    Initial commit: Project structure and basic configuration

---
+++ src/main/java/ai/lingualeap/lingualeap/LingualeapApplication.java
@@ -0,0 +1,13 @@
+package ai.lingualeap.lingualeap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingualeapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingualeapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/lingualeap/lingualeap/LingualeapApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.lingualeap.lingualeap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingualeapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
